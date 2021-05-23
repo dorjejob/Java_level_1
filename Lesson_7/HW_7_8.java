4c39febc913d7c4721ebe29e3665c6994de0b6b6
dorjejob <dorjejob@gmail.com>
2021-05-23 23:15:25 +0300

    ДЗ №7

---
+++ Lesson_7/HW_7_8.java
@@ -0,0 +1,10 @@
+package Java_level_1.git.Lesson_7;
+
+import javax.swing.*;
+
+public class HW_7 {
+    public class mainWondow extends JFrame {
+
+    }
+
+}
---
+++ Lesson_7/MainWindow.java
@@ -0,0 +1,2 @@
+package Java_level_1.git.Lesson_7;public class MainWindow {
+}
